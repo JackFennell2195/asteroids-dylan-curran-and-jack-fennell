4135f376131dbda7840fbd2ce7dca60791abf706
(Student) - Dylan Curran <C00217112@ITCARLOW.IE>
2017-03-28 14:03:14 +0100

    This should be fixed

---
+++ GameAsteroids/GameAsteroids/Gameplay.cpp
@@ -0,0 +1,32 @@
+#include "Gameplay.h"
+#include "Game.h"
+Gameplay::Gameplay()
+{
+}
+
+Gameplay::~Gameplay()
+{
+}
+
+void Gameplay::initialise()
+{
+}
+
+void Gameplay::render(sf::RenderWindow &)
+{
+}
+
+void Gameplay::takeInput(sf::Event event)
+{
+	if (event.type == sf::Event::KeyPressed)
+	{
+		if (event.key.code == sf::Keyboard::Up)
+		{
+			std::cout << "You just pressed the up button" << std::endl;
+		}
+	}
+}
+
+void Gameplay::update(sf::Time)
+{
+}
